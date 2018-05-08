e5ec583957961b15dbc72ba1561c83d0ec9599c1
Ahmed Kamel <ak@ahmedkamel.net>
2018-05-07 17:41:20 -0700

    updae build struture

--- config/default.js
+++ config/default.js
@@ -1,8 +1,11 @@
-var bytes = require('bytes');
+var bytes = require('bytes')
+  , ms    = require('ms')
+  ;
 
 module.exports = {
   "codedb" : {
     "file_size_limit"   : bytes('10kb'),
-    "git_archive_limit" : bytes('5mb')
+    "git_archive_limit" : bytes('5mb'),
+    "ttl"               : ms('1h') / 1000
   }
 };
