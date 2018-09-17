ee3c96eaced3dee96163cb13b8f55e01f5e0a9f9
Ahmed Kamel <ak@ahmedkamel.net>
2018-09-17 00:54:28 -0700

    remove unused config

--- config/default.js
+++ config/default.js
@@ -5,7 +5,6 @@ var bytes = require('bytes')
 module.exports = {
   "codedb" : {
     "file_size_limit"   : bytes('10kb'),
-    "git_archive_limit" : bytes('5mb'),
     "ttl"               : ms('10m')
   }
 };
