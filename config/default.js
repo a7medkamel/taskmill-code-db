236750b753df9b1a499ac18d63bedcbb7058422b
Ahmed Kamel <ahmed.kamel@gmail.com>
2018-10-02 15:43:46 -0700

    initial attempt at docker in docker + move the key configs to config files

--- config/default.js
+++ config/default.js
@@ -1,10 +1,15 @@
 var bytes = require('bytes')
   , ms    = require('ms')
+  , fs    = require('fs')
   ;
 
 module.exports = {
   "codedb" : {
     "file_size_limit"   : bytes('10kb'),
-    "ttl"               : ms('10m')
+    "ttl"               : ms('10m'),
+    "git-crypt"         : {
+      "key"         : fs.readFileSync('./key/pgp.asc', 'utf-8'),
+      "passphrase"  : fs.readFileSync('./key/pgp.passphrase', 'utf-8')
+    }
   }
 };
